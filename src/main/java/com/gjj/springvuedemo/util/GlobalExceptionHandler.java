a075ccf3de780dc4b26a35cd6eff35daf04c6bbb
genjigjj <18812674687@163.com>
2018-04-20 17:22:07 +0800

    修改response编码为utf8

--- src/main/java/com/gjj/springvuedemo/util/GlobalExceptionHandler.java
+++ src/main/java/com/gjj/springvuedemo/util/GlobalExceptionHandler.java
@@ -5,7 +5,6 @@ import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authz.AuthorizationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,8 +54,8 @@ public class GlobalExceptionHandler {
      * @author gjj
      * @date 2018-04-20
      */
-    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public JSONObject requestExceptionHandler(HttpRequestMethodNotSupportedException e){
+    @ExceptionHandler(Exception.class)
+    public JSONObject requestExceptionHandler(Exception e){
         JSONObject jsonObject = JsonUtil.returnJson(ResultEnum.BAD_REQUEST,null);
         logger.info("请求失败",e);
         return jsonObject;
